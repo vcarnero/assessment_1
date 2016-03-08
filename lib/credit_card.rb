60f5ed49dd61b58579dd5d240ae107b8a7250682
Veronica Carnero <vcarnero@gmail.com>
2016-03-07 23:03:43 -0700

    Added the payment method to CreditCard class
    [#115221863]

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -8,4 +8,13 @@ class CreditCard
   def charge(amount)
     @balance += amount
   end
+
+  def payment(amount)
+    @balance -= amount
+    if @balance > 0
+      puts "A remaining balance of #{@balance} is left."
+    else
+      puts 'Thank you'
+    end
+  end
 end
